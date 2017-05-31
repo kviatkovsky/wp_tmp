49490490eb8fad5ca6217fba148059344d31dc1e
kviatkovsky.v <kviatkovsky.v@gmail.com>
2017-05-31 17:45:42 +0300

    paralax,menu,logo

--- ht/js/common.js
+++ ht/js/common.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-
-	
-	
+  $(".main_head").css("min-height", $(window).height());
 });
+$(".toggle_mnu").click(function(){
+	if($(".top_mnu").is(":visible")){
+		$(".top_mnu").fadeOut(600);
+} else {
+	$(".top_mnu").fadeIn(600);
+}
+});
+
+$(".toggle_mnu, .menu_item").click(function() {
+  $(".sandwich").toggleClass("active");
+});
+
+$(window).load(function() { 
+	$(".loader_inner").fadeOut(); 
+	$(".loader").delay(400).fadeOut("slow"); 
+});
+
